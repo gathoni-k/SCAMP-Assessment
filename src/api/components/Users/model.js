8cd0528509b37b722f8307264cd20a45a90b516a
gathoni-k <maryg.kahiga@gmail.com>
2020-04-30 05:54:02 +0300

    Create User model

---
+++ src/api/components/Users/model.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ['salesPerson', 'admin']
+  }
+});
+
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified(this.password)) next();
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(this.password, salt);
+  this.password = hash;
+  next();
+});
+
+module.exports = mongoose.model('User', UserSchema);
