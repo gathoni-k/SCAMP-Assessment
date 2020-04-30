6a1c433451d6a3526a0971302264ff8b25ad5f5b
gathoni-k <maryg.kahiga@gmail.com>
2020-04-30 11:39:54 +0300

    Configure signup route

--- src/app.js
+++ src/app.js
@@ -7,4 +7,12 @@ require('./db')(MONGOURI);
 
 const app = express();
 
+// require routes
+const routes = require('./api/routes');
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/api/user', routes);
+
 module.exports = app;
