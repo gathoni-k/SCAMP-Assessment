937715c006e577215fe55502d82ff3c75b1b38fe
gathoni-k <maryg.kahiga@gmail.com>
2020-04-30 11:38:58 +0300

    Clean database after tests

--- __tests__/users.js
+++ __tests__/users.js
@@ -102,6 +102,28 @@ async function removeAllCollections() {
   }
 }
 
+async function dropAllCollections () {
+  const collections = Object.keys(mongoose.connection.collections)
+  for (const collectionName of collections) {
+    const collection = mongoose.connection.collections[collectionName]
+    try {
+      await collection.drop()
+    } catch (error) {
+      if (error.message === 'ns not found') return
+      if (error.message.includes('a background operation is currently running')) return
+      console.log(error.message)
+    }
+      console.log(error.message)
+      return
+    }
+  }
+}
+
 afterEach(async () => {
   await removeAllCollections();
+});
+
+afterAll(async () => {
+  await dropAllCollections();
+  await mongoose.connection.close();
 });
