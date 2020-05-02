9bd4e5b5685339c9c8fa258e55e79621d0c271c6
gathoni-k <maryg.kahiga@gmail.com>
2020-05-02 14:07:03 +0300

    Add db helpers for product routes

---
+++ src/api/components/Products/db.js
@@ -0,0 +1,54 @@
+const Product = require('./model');
+
+module.exports = {
+  createProduct: async (product) => {
+    try {
+      const { productName,
+        productLabel,
+        startingInventory,
+        inventoryReceived,
+        inventorySold,
+        inventoryOnHand,
+        minimumRequired } = product;
+      // create new Product
+      const newProduct = new Product({
+        productName,
+        productLabel,
+        startingInventory,
+        inventoryReceived,
+        inventorySold,
+        inventoryOnHand,
+        minimumRequired
+      });
+      await newProduct.save();
+      return newProduct;
+    } catch (error) {
+      return false;
+    }
+  },
+  deleteProduct: async (id) => {
+    try {
+      await Product.findByIdAndDelete(id);
+      return true;
+    } catch (error) {
+      return false;
+    }
+  },
+  updateProduct: async (update, id) => {
+    try {
+      await Product.findOneAndUpdate({ _id: id }, { $set: update });
+      const updated = await Product.findById(id);
+      return updated;
+    } catch (error) {
+      return false;
+    }
+  },
+  getProduct: async (id) => {
+    try {
+      const product = await Product.findById(id);
+      return product;
+    } catch (error) {
+      return false;
+    }
+  }
+};
