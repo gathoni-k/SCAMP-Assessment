e314728ec5c444e6fd163ad216ae42a1bd4a1aab
gathoni-k <maryg.kahiga@gmail.com>
2020-05-05 04:22:07 +0300

    Implement role based authentication for product routes

--- src/api/components/Products/routes.js
+++ src/api/components/Products/routes.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const controllers = require('./controllers');
+const { isAuthorized, isAdmin } = require('../../helpers.');
 
 router.get('/all', controllers.getProducts);
 router.get('/one/:productId', controllers.getProduct);
-router.post('/create', controllers.createProduct);
-router.post('/update/:productId', controllers.updateProduct);
-router.delete('/delete/:productId', controllers.deleteProduct);
+router.post('/create', isAuthorized, isAdmin, controllers.createProduct);
+router.post('/update/:productId', isAuthorized, isAdmin, controllers.updateProduct);
+router.delete('/delete/:productId', isAuthorized, isAdmin, controllers.deleteProduct);
 
 module.exports = router;
