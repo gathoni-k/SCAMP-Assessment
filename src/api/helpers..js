6e69fd9d0e8567f110625ed1f0ffb469b5536972
gathoni-k <maryg.kahiga@gmail.com>
2020-05-05 04:21:28 +0300

    Add jwt role based authentication

--- src/api/helpers..js
+++ src/api/helpers..js
@@ -4,6 +4,29 @@ require('dotenv').config();
 const { JWT_SECRET } = process.env;
 module.exports = {
   genToken: (user) => jwt.sign({
-    payload: user.id
-  }, JWT_SECRET, { expiresIn: '1h' })
+    id: user.id,
+    role: user.role
+  }, JWT_SECRET, { expiresIn: '1h' }),
+  isAuthorized: (req, res, next) => {
+    try {
+      const token = req.headers.authorization.split(' ')[1];
+      if (!token) {
+        return res.status(401).json({ message: 'Please login' });
+      }
+      const decoded = jwt.verify(token, JWT_SECRET);
+      req.user = decoded;
+      next();
+    } catch (error) {
+      return res.status(401).json({ message: 'Invalid token' });
+    }
+  },
+  isAdmin: (req, res, next) => {
+    try {
+      const { role } = req.user;
+      if (role !== 'admin') throw new Error();
+      next();
+    } catch (error) {
+      return res.status(401).json({ message: 'Unauthorized, admins only' });
+    }
+  },
 };
